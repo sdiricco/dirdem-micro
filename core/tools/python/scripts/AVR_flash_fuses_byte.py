5f263331b9e58353b1e479e1ad4c30b543c46465
Simone Di Ricco <simone.diricco.1@gmail.com>
2020-04-18 17:19:09 +0200

    "prova"

--- core/tools/python/scripts/AVR_flash_fuses_byte.py
+++ core/tools/python/scripts/AVR_flash_fuses_byte.py
@@ -13,15 +13,8 @@
 ####################################################################################################################################################
 import os
 import sys
-<<<<<<< HEAD
 from AVR_pathTool import absolutePathFile_avrdude_exe
-=======
-from subprocess import Popen
 
-import asyncio
-import time
-
->>>>>>> 904403b23e7356494223f811d5b5ec133d5f1c12
 
 ####################################################################################################################################################
 # Global variables                                                                                                                                 #
@@ -35,7 +28,6 @@ sysArg_programmer         = sys.argv[2]
 sysArg_string_low_fuse    = sys.argv[3]
 sysArg_string_high_fuse   = sys.argv[4]
 
-<<<<<<< HEAD
 #Example:
 # Internal RC OSC: 1Mhz
 # >>> AVR_flash_fuses_byte.py m32 usbasp 0x61 0xDE 
@@ -43,20 +35,10 @@ sysArg_string_high_fuse   = sys.argv[4]
 # >>> AVR_flash_fuses_byte.py m32 usbasp 0x64 0xDE
 # WARNIG!
 # Do not set EXT OSC if it is not connected!
-=======
-#Esempio:
-# microcontroller_tag: m32
-# programmer: usbasp
-# low_fuse: 0x64
-# high_fuse: 0xDE
-# lanciare il seguente comando, il quale chiamerà lo script "AVR_flash_fuses_byte.py" con i paramentri impostati sopra dall'esempio.
-# >>> AVR_flash_fuses_byte.py m32 usbasp 0x64 0xDE 
->>>>>>> 904403b23e7356494223f811d5b5ec133d5f1c12
 
 ####################################################################################################################################################
 # Global assignments                                                                                                                               #
 ####################################################################################################################################################
-<<<<<<< HEAD
 
 argsFlashFuseByte_avrdude      = " -u" + \
                                  " -c " + sysArg_programmer + \
@@ -66,15 +48,6 @@ argsFlashFuseByte_avrdude      = " -u" + \
                                  " -v -v " + \
                                  " -l " + "AVR_FlashFusesByte_logfile.txt"
 
-=======
-path_avrdude    = "avrdude.exe"
-#flagsFlash_avrdude      = " -u" + " -c " + programmer + " -p " +  microcontroller_tag + " -U " + "lfuse:w:"+ low_fuse + ":m " + " -U " + "hfuse:w:" + high_fuse + ":m "
-flagsFlash_avrdude      = " -u" + " -c " + programmer + " -p " +  microcontroller_tag + " -U " + "lfuse:w:"+ low_fuse + ":m " + " -U " + "hfuse:w:" + high_fuse + ":m " "-n " "-l " "logfile.txt"
-#..\\..\\avrdude\\avrdude.exe -u -c usbasp -p m32 -U lfuse:w:0x64:m -U hfuse:w:0xDE:m
-#C:\Repository\dirdem-micro\core\tools\python\scripts>..\\..\\avrdude\\avrdude.exe -u -c usbasp -p m32 -U lfuse:w:0x64:m -U hfuse:w:0xDE:m -n -l C:\Users\39347\Desktop\Nu\logfile.txt
-
-#
->>>>>>> 904403b23e7356494223f811d5b5ec133d5f1c12
 ####################################################################################################################################################
 # Global Function                                                                                                                                  #
 ####################################################################################################################################################
@@ -82,25 +55,17 @@ flagsFlash_avrdude      = " -u" + " -c " + programmer + " -p " +  microcontrolle
 ####################################################################################################################################################
 # Script                                                                                                                                           #
 ####################################################################################################################################################
-<<<<<<< HEAD
 print("Calling AVRDUDE..")
 print(absolutePathFile_avrdude_exe + argsFlashFuseByte_avrdude)
 os.system(absolutePathFile_avrdude_exe + argsFlashFuseByte_avrdude)
 print("End Process")
-=======
-print("Calling AVRDUDE..\n>>>" + path_avrdude + flagsFlash_avrdude)
->>>>>>> 904403b23e7356494223f811d5b5ec133d5f1c12
 
 
 
 #import asyncio
 #from subprocess import Popen
 #p = Popen(['watch', 'ls']) # something long running
-<<<<<<< HEAD
 #os.system(absolutePathFile_avrdude_exe + argsFlashFuseByte_avrdude)
-=======
-#os.system(path_avrdude + flagsFlash_avrdude)
->>>>>>> 904403b23e7356494223f811d5b5ec133d5f1c12
 #p.terminate()
 
 #async def run(cmd):
@@ -117,12 +82,5 @@ print("Calling AVRDUDE..\n>>>" + path_avrdude + flagsFlash_avrdude)
 #    if stderr:
 #        print(f'[stderr]\n{stderr.decode()}')
 
-<<<<<<< HEAD
 #asyncio.run(run(absolutePathFile_avrdude_exe + argsFlashFuseByte_avrdude))
 
-=======
-#asyncio.run(run(path_avrdude + flagsFlash_avrdude))
-os.system(path_avrdude + flagsFlash_avrdude)
-time.sleep(1)
-print("Finito!!")
->>>>>>> 904403b23e7356494223f811d5b5ec133d5f1c12
