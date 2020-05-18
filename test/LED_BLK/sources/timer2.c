59353a453ec9931b0d6823a392f403550e40f849
Simone Di Ricco <simone.diricco.1@gmail.com>
2020-05-18 23:39:23 +0200

    prova

--- test/LED_BLK/sources/timer2.c
+++ test/LED_BLK/sources/timer2.c
@@ -1,6 +1,6 @@
 
 #include <include/LEDblink.h>
-#include <avr/io.h> 
+#include <avr/io.h>
 
 void Init_Timer2()
 {
@@ -8,4 +8,12 @@ void Init_Timer2()
 	TMR_2_AS_REG = (1 << AS2);					//Attivo ingresso clock asincrono (quarzo 32768Hz)
 	TMR_MSK_REG  = (1 << TOIE2);				//Abilito interrupt ogni Overflow
 	TMR_2_CNT = 0;
+}
+
+if (/* condition */) {
+	/* code */
+} else if (/* condition */) {
+	/* code */
+} else {
+	/* code */
 }
